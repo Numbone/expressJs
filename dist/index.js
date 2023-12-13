2667abf8111c32bdaa55290b3d1654816b036f5b
Nurlybek <nurlybekqwerty@gmail.com>
2023-12-13 17:45:51 +0600

    Add HTTP Status

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,13 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 8080;
 app.use(express_1.default.json());
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404
+};
 let db = [
     { id: 1, title: 'Course Title' },
     { id: 2, title: 'Course Title2' },
@@ -15,7 +22,7 @@ let db = [
 ];
 app.get('/', (req, res) => {
     res.send(JSON.stringify(db));
-    res.sendStatus(200);
+    res.sendStatus(HTTP_STATUSES.OK_200);
     res.json({ message: 'send message' });
     res.send();
 });
@@ -25,7 +32,7 @@ app.get('/courses', (req, res) => {
 app.get('/courses/:id', (req, res) => {
     const findCourses = db.find(item => item.id === +req.params.id);
     if (!findCourses) {
-        res.send(404);
+        res.send(HTTP_STATUSES.NOT_FOUND);
         return;
     }
     res.json(findCourses);
@@ -35,27 +42,27 @@ app.post('/courses', (req, res) => {
         id: +(new Date()),
         title: req.body.title
     });
-    res.sendStatus(201).json({
+    res.sendStatus(HTTP_STATUSES.CREATED).json({
         id: +(new Date()),
         title: req.body.title
     });
 });
 app.delete('/courses/:id', (req, res) => {
     db = db.filter(item => item.id === +req.params.id);
-    res.sendStatus(204);
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT);
 });
 app.put('/courses/:id', (req, res) => {
     if (!req.body.title) {
-        res.sendStatus(400);
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST);
         return;
     }
     const findCourses = db.find(item => item.id !== +req.params.id);
     if (!findCourses) {
-        res.send(404);
+        res.send(HTTP_STATUSES.BAD_REQUEST);
         return;
     }
     findCourses.title = req.body.title;
-    res.sendStatus(204);
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT);
 });
 app.listen(PORT, () => {
     console.log('listening on 8080');
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import  express from 'express';
 const app = express();
 const PORT= 8080;
 app.use(express.json());
+
+const HTTP_STATUSES={
+    OK_200:200,
+    CREATED:201,
+    NO_CONTENT:204,
+
+    BAD_REQUEST:400,
+    NOT_FOUND:404
+}
 let db = [
     { id: 1, title: 'Course Title' },
     { id: 2, title: 'Course Title2' },
@@ -11,7 +20,7 @@ let db = [
 
 app.get('/', (req, res) => {
     res.send(JSON.stringify(db))
-    res.sendStatus(200);
+    res.sendStatus(HTTP_STATUSES.OK_200);
     res.json({message:'send message'});
     res.send();
 })
@@ -23,7 +32,7 @@ app.get('/courses', (req, res) => {
 app.get('/courses/:id', (req, res) => {
     const findCourses =db.find(item => item.id === +req.params.id);
     if (!findCourses){
-        res.send(404);
+        res.send(HTTP_STATUSES.NOT_FOUND);
         return 
     }
     res.json(findCourses);
@@ -35,7 +44,7 @@ app.post('/courses', (req, res) => {
     id:+(new Date()),
     title:req.body.title
    })
-   res.sendStatus(201).json({
+   res.sendStatus(HTTP_STATUSES.CREATED).json({
     id:+(new Date()),
     title:req.body.title
    })
@@ -43,26 +52,26 @@ app.post('/courses', (req, res) => {
 
 app.delete('/courses/:id', (req, res) => {
     db=db.filter(item => item.id === +req.params.id);
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT)
    
 })
 
 
 app.put('/courses/:id', (req, res) => {
     if (!req.body.title){
-        res.sendStatus(400)
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST)
         return
     }
 
     const findCourses =db.find(item => item.id !== +req.params.id);
     if (!findCourses){
-        res.send(404);
+        res.send(HTTP_STATUSES.BAD_REQUEST);
         return 
     }
 
     findCourses.title=req.body.title;
 
-    res.sendStatus(204)
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT)
 })
 
 app.listen(PORT, () => {
