bc1a2617de113be18c368ba06facad9acd435cb7
Nurlybek <nurlybekqwerty@gmail.com>
2024-05-01 02:46:17 +0500

    add login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,17 +1,60 @@
 import { Request, Response } from 'express';
 import { pool } from '../db/db';
 import { User } from './../types/user';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+const generateJwt = (id:number, email:string, role:string) => {
+    jwt.sign({ id, email, role }, process.env.SECRET_KEY as string ,{ expiresIn: '24h' })
+}
 class UserController {
     async registration(req:  Request<Record<string, never>, unknown, User>, res: Response) {
-        const { email, password ,id,role} = req.body;
+        const { email, password} = req.body;
         if (!email || !password) {
             return res.status(400).json({ message: 'All fields are required' });
         }
+        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (user.rows.length > 0) {
+            return res.status(409).json({ message: 'User already exists' });
+        }
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10); // 10 is the saltRounds
+            await pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword]);
+            return res.status(201).json({ message: 'User created successfully' });
+        } catch (error) {
+            console.error('Error during user registration:', error);
+            return res.status(500).json({ message: 'Internal server error' });
+        }       
+    }
 
-        await pool.query('INSERT INTO user (email, password) VALUES ($1, $2)', [email, password]);
-        return res.status(201).json({ message: 'User created successfully' });
+    async login(req:Request<Record<string,never>,unknown,User>,res:Response){
+        const {email,password}=req.body
+
+        if(!email || !password){
+            res.json({
+                message:'All fields are required'
+            })
+        }
+
+        const user = await pool.query<User>('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (user.rows.length === 0) {
+            return res.status(401).json({message:'Invalid email'})
+        }   
+
        
+        const comparePassword = bcrypt.compareSync(password, user.rows[0].password);
+        if (!comparePassword) {
+            return res.json({
+                message: 'Invalid password'
+            })
+        }
+
+       
+
+        const token = generateJwt(user.rows[0].id, user.rows[0].email, user.rows[0].role)
+        return res.json({ token })
+        
     }
 }
 
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/user.controller";
 const UserController=new userController()
 const userRouter =Router();
 userRouter.post('/registration',UserController.registration);
+userRouter.post('/login',UserController.login);
 
 export {userRouter}
